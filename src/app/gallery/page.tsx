b2b5b5ba15fa15600578a6c60b154e8597834cac
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-08-15 18:28:36 +0400

    show images in gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,44 +1,37 @@
-"use client"
+import cloudinary from "cloudinary"
+import CloudinaryImage from "./CloudinaryImage"
+import UploadButton from "./UploadButton"
 
-import { useState } from "react"
-import { CldUploadButton } from "next-cloudinary"
+type SearchResult = {
+  public_id: string
+}
 
-import { UploadType } from "../page"
-import { Button } from "@/components/ui/button"
-
-export default function GalleryPage() {
-  const [imageId, setImageId] = useState("")
+export default async function GalleryPage() {
+  const result = (await cloudinary.v2.search
+    .expression("resource_type:image")
+    .sort_by("created_at", "desc")
+    .max_results(30)
+    .execute()) as { resources: SearchResult[] }
 
   return (
     <section>
-      <div className="flex justify-between">
-        <h1 className="text-4xl font-bold">Gallery</h1>
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h1 className="text-4xl font-bold">Gallery</h1>
+          <UploadButton />
+        </div>
 
-        <Button asChild>
-          <div className="flex gap-2 items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-              />
-            </svg>
-
-            <CldUploadButton
-              onUpload={(result: UploadType | any) => {
-                setImageId(result.info.public_id)
-              }}
-              uploadPreset="up0lliqg"
+        <div className="grid grid-cols-4 gap-4">
+          {result.resources.map((result) => (
+            <CloudinaryImage
+              key={result.public_id}
+              src={result.public_id}
+              width="400"
+              height="300"
+              alt="cloudinary image"
             />
-          </div>
-        </Button>
+          ))}
+        </div>
       </div>
     </section>
   )
